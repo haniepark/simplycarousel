da5632a1e26a802d1564a503d6b57c3149e07d71
Stephanie Park <haniepark@gmail.com>
2013-11-08 14:33:38 -0500

    skeleton work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-	
+	$('a[href*=#]').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top -80
+	    }, 500);
+	    return false;
+	});
 	$('.carousel-slide').slider();
 	
 });
