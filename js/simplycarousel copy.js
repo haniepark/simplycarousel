7a960afc6a097fa679d51bce4704ea4af30877bc
Stephanie Park <haniepark@gmail.com>
2013-11-11 16:18:57 -0500

    building demo carousels

--- js/simplycarousel copy.js
+++ js/simplycarousel copy.js
@@ -3,16 +3,15 @@
 		var q = $.fn.slider.settings = $.extend({}, $.fn.slider.defaults, options),
 		slide = this,
 		total_time = q.transition_time + q.next_slides,
-		interval,
-		on = 'on';
+		interval;
 		function carousel(){
 			var i = $(q.slideOn).index();    
-		    slide.eq(i).removeClass(on).fadeOut(q.transition_time);	
+		    slide.eq(i).removeClass(q.on).fadeOut(q.transition_time);	
 		    if (i == slide.length-1 ){ i = -1 }; // loop to start
-		    slide.eq(i + 1).fadeIn(q.transition_time).addClass(on);
-		}		
+		    slide.eq(i + 1).fadeIn(q.transition_time).addClass(q.on);
+		}
 		slide.fadeOut();	
-		slide.first().addClass(on).fadeIn(q.transition_time);
+		slide.first().addClass(q.on).fadeIn(q.transition_time);
 		// set carousel interval
 		interval = setInterval(carousel, total_time);
 	}			
@@ -20,49 +19,48 @@
     $.fn.slider.defaults = {
         transition_time : 1000,
         next_slides : 2500,
-		slideOn :'.carousel-slide.on'
+        on : 'on',
+        slideOn : '.carousel-slide.on'
     };   
 	$.fn.sliderArrow = function(options){
 		var q = $.fn.sliderArrow.settings = $.extend({}, $.fn.sliderArrow.defaults, options),
 		slide = this,
 		total_time = q.transition_time + q.next_slides,
 		interval,
-		container = '#main_banner',
-		right = this.closest(container).find('.arrow.right'),
-		left = this.closest(container).find('.arrow.left'),
-		arrow = '.arrow',
-		on = 'on',
-		slideOn = '.carousel-slide.on';				
+		right = this.closest(q.container).find(q.rightArrow),
+		left = this.closest(q.container).find(q.leftArrow);
+		
 		function carousel(){
 			var i = $(q.slideOn).index();    
-			slide.eq(i).removeClass(on).fadeOut(q.transition_time);	
+			slide.eq(i).removeClass(q.on).fadeOut(q.transition_time);	
 	     	if (i == slide.length-1 ){ i = -1 }; // loop to start
-		 	slide.eq(i + 1).fadeIn(q.transition_time).addClass(on);
+		 	slide.eq(i + 1).fadeIn(q.transition_time).addClass(q.on);
+
 		}
 		slide.fadeOut();	
-		slide.first().addClass(on).fadeIn(q.transition_time);
+		slide.first().addClass(q.on).fadeIn(q.transition_time);
 		// set carousel interval
 		interval = setInterval(carousel, total_time);	
 		
 		right.on('click', function(){carousel();});
 		left.on('click', function(){
 			var i =$(q.slideOn).index();
-		    slide.eq(i).removeClass(on).fadeOut(q.transition_time);    
+		    slide.eq(i).removeClass(q.on).fadeOut(q.transition_time);    
 			if (i < 0 ){ i = slide.length -1 }; // loop to start	
-			slide.eq(i -1).fadeIn(q.transition_time).addClass(on); 
+			slide.eq(i -1).fadeIn(q.transition_time).addClass(q.on); 
 		});
-		$(arrow).hide();
-		$(container).hover(function(){	
+		$(q.arrow).hide();
+		$(q.container).hover(function(){	
 			clearTimeout(interval);
-			$(arrow).show();
+			$(q.arrow).show();
 		}, function(){
 			interval = setInterval(carousel, total_time);
-			$(arrow).hide()
+			$(q.arrow).hide()
 		});	
 		//window loaded @ < 480px;
 		if($(window).width() <= 480) {
 			clearTimeout(interval);
-			$(container).hover(function(){	
+			$(q.container).hover(function(){	
 			}, function(){
 				clearTimeout(interval);
 			});
@@ -70,14 +68,14 @@
 		$(window).on('resize', function(){
 			if($(window).width() <= 480) {
 				clearTimeout(interval);
-				$(container).hover(function(){	
+				$(q.container).hover(function(){	
 				}, function(){
 					clearTimeout(interval);
 				});				
 			} else {
 				clearTimeout(interval);
 				interval = setInterval(carousel, total_time);
-				$(container).mouseover(function(){
+				$(q.container).mouseover(function(){
 				});
 			}	 
 		});
@@ -86,7 +84,12 @@
     $.fn.sliderArrow.defaults = {
         transition_time : 1000,
         next_slides : 2500,
-		slideOn :'.carousel-slide.on'
+        on : 'on',
+        slideOn : '.carousel-slide.on',
+        container: '.container',
+        arrow: '.arrow',
+        rightArrow: '.arrow.right',
+        leftArrow: '.arrow.left'
     };   
 }(jQuery, window, document));
 
